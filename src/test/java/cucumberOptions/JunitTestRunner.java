685d470634151efea68910a1174a0e689579abc3
Iván Zucchi <81567212+ivanzucchig@users.noreply.github.com>
2022-12-08 21:06:41 +0100

    version con JunitTestRunner

---
+++ src/test/java/cucumberOptions/JunitTestRunner.java
@@ -0,0 +1,18 @@
+package cucumberOptions;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",
+        glue = "stepDefinitions",
+        monochrome = true,
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber.html", "json:target/cucumber.json",
+                "rerun:target/failed_scenario.txt"}
+)
+public class JunitTestRunner  {
+
+}
---
+++ src/test/java/stepDefinitions/CheckoutPageStepDefinition.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pageObjects.CheckoutPage;
+import utils.TestContextSetup;
+
+
+public class CheckoutPageStepDefinition {
+    public CheckoutPage checkoutPage;
+    TestContextSetup testContextSetup;
+
+    public CheckoutPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+        this.checkoutPage = testContextSetup.pageObjectManager.getCheckoutPage();
+    }
+
+    @Then("^user proceeds to checkout and validate the (.+) items in checkout page$")
+    public void user_proceeds_to_checkout_and_validate_the_items_in_checkout_page(String name) {
+        checkoutPage.CheckoutItems();
+
+    }
+
+
+    @And("^verify user has ability to enter promo code and place the order$")
+    public void verify_user_has_ability_to_enter_promo_code_and_place_the_order()  {
+
+        Assert.assertTrue(checkoutPage.VerifyPromoBtn());
+        Assert.assertTrue(checkoutPage.VerifyPlaceOrder());
+    }
+}
